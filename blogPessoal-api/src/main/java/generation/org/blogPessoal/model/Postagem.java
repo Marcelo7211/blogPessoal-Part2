3e8abe6255040504bbfbacc34c8ac65ae46dae59
celin <marcelo.barboza.cmkp@gmail.com>
2020-07-07 15:27:48 -0300

    Feito Relacionamento entre tabalas

--- blogPessoal-api/src/main/java/generation/org/blogPessoal/model/Postagem.java
+++ blogPessoal-api/src/main/java/generation/org/blogPessoal/model/Postagem.java
@@ -6,12 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
@@ -32,32 +33,49 @@ public class Postagem {
 
 	@Temporal(TemporalType.TIMESTAMP)
     private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public long getId() {
 		return id;
 	}
+	
 	public void setId(long id) {
 		this.id = id;
 	}
+	
 	public String getTitulo() {
 		return titulo;
 	}
+	
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+	
 	public String getTexto() {
 		return texto;
 	}
+	
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+	
 	public Date getData() {
 		return data;
 	}
+	
 	public void setData(Date data) {
 		this.data = data;
 	}
 	
+	public Tema getTema() {
+		return tema;
+	}
 	
-	
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+		
 }
